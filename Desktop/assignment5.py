d8e1017ba9d1e46aa3e4f92cfcfadab52c9228ae
Zuse <zuse@zuse-debian.2n2.org>
2019-03-05 10:44:44 -0600

    Begin basic addition/subtraction program

---
+++ Desktop/assignment5.py
@@ -0,0 +1,7 @@
+first_number = int(input("Please enter a postive number"))
+second_number = int(input("Please enter another positive number"))
+if first_number <0 or second number <0:
+    print("Error! Invalid input!")
+else:
+    print(first_number, "+", second_number, "=", (first_number+second_number))
+    print(first_number, "-", second_number, "=", (first_number-second_number))
