682ff22f34a2690836af45700200a7adbc63379e
n-east <neast@student.waubonsee.edu>
2019-03-05 12:08:32 -0600

    completed assignment5

--- Desktop/assignment5.py
+++ Desktop/assignment5.py
@@ -1,7 +1,11 @@
-first_number = int(input("Please enter a postive number"))
-second_number = int(input("Please enter another positive number"))
-if first_number <0 or second number <0:
-    print("Error! Invalid input!")
+first = input("Enter one positive number: ")
+print(first)
+second = input("Enter a second positive number: ")
+print(second)
+if int(first) < 0 or int(second) < 0:
+                 print("You need to enter a POSITIVE number.")
 else:
-    print(first_number, "+", second_number, "=", (first_number+second_number))
-    print(first_number, "-", second_number, "=", (first_number-second_number))
+    print("The sum of the numbers you entered is: ", (int(first) + int(second)))
+    print("The result of subtracting", second, "from", first, "is", (int(first) - int(second)))
+    print("The result of multiplying the numbers you entered is ", (int(first) * int(second)))
+    print("The result of dividing", first, "by", second, "is", (int(first)/int(second)))
