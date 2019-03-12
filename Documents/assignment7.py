e24c106c0a4d8f5529c99441aa21b1c655045c9b
n-east <neast@student.waubonsee.edu>
2019-03-12 12:10:41 -0500

    added addition functionality

--- Documents/assignment7.py
+++ Documents/assignment7.py
@@ -4,11 +4,14 @@ first_pair = int(first_num / second_num)
 
 print(first_num, "divided by" , second_num , "is equal to :", first_pair)
 print("The remainder is" , (first_num % second_num))
+print(first_num, "+" , second_num , "is equal to: ", (first_num + second_num))
 
 first_num_two = int(input("Please enter one number:"))
 second_num_two = int(input("Please enter anoher number: "))
 second_pair = int(first_num_two / second_num_two)
 print(first_num_two, "divided by" , second_num_two , "is equal to :", second_pair)
 print("The remainder is" , (first_num_two % second_num_two))
+print(first_num_two, "+" , second_num_two , "is equal to: ", (first_num_two + second_num_two))
+
 
 print("The product of", first_pair, "and" , second_pair , "is" , (first_pair * second_pair))
