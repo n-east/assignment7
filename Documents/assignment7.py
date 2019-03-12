82f4e193d3673096686724b297482fa4b367ca59
n-east <neast@student.waubonsee.edu>
2019-03-12 11:35:08 -0500

    Added division funcitionality for second pair of numbers

--- Documents/assignment7.py
+++ Documents/assignment7.py
@@ -1,5 +1,12 @@
 first_num = int(input("Please enter one number: "))
 second_num = int(input("Please enter another number: "))
-print(first_num, "divided by" , second_num , "is equal to :")
-print(int(first_num / second_num))
+first_pair = int(first_num / second_num)
+
+print(first_num, "divided by" , second_num , "is equal to :", first_pair)
 print("The remainder is" , (first_num % second_num))
+
+first_num_two = int(input("Please enter one number:"))
+second_num_two = int(input("Please enter anoher number: "))
+second_pair = int(first_num_two / second_num_two)
+print(first_num_two, "divided by" , second_num_two , "is equal to :", second_pair)
+print("The remainder is" , (first_num_two % second_num_two))
